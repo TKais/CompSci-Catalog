622062e701926b9262cef4d9e9406e32a22d0718
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-11-17 11:42:37 -0800

    feat: Add shell script to install dependencies

---
+++ vagrant/config.sh
@@ -0,0 +1,11 @@
+apt-get -qqy update
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+apt-get -qqy install python-sqlalchemy
+apt-get -qqy install python-pip
+pip3 install --upgrade pip
+pip3 install werkzeug --user
+pip3 install flask --user
+pip3 install Flask-Login==0.1.3 --user
+pip3 install oauth2client --user
+pip3 install requests --user
+pip3 install httplib2 --user
