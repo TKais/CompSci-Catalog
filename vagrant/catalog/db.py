356a0f44c00a658a247b76a3158a713c2c03c065
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-11-19 08:48:53 -0800

    feat: Add initial database class for topics

---
+++ vagrant/catalog/db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Topic(Base):
+  __tablename__ = 'topic'
+
+  id = Column(Integer, primary_key = True)
+  name = Column(String(250), nullable = False)
+
+  @property
+  def serialize(self):
+    return {
+      id: self.id,
+      name: self.name
+    }
