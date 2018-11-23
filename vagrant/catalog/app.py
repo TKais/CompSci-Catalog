1774d2b89bab3f96d806f60a2d23a43d26195559
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-11-23 08:50:52 -0800

    feat: Add rest of required routes

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -15,23 +15,54 @@ def show_topic(topic_name):
   print('A particular topic page')
 
 
-@app.route('/topics/<topic_name>/category/<category_name>/new/', methods=['GET', 'POST'])
+@app.route('/topics/<topic_name>/categories/')
+def show_categories(topic_name):
+  print('A set of categories within a particular topic')
+
+
+@app.route('/topics/<topic_name>/categories/new/')
+def create_category(topic_name):
+  print('new category')
+
+
+@app.route('/topics/<topic_name>/categories/<category_name>/')
+def show_category(topic_name, category_name):
+  print('A particular category page')
+
+
+@app.route('/topics/<topic_name>/categories/<category_name>/new/', methods=['GET', 'POST'])
 def create_article(topic_name, category_name):
   print('new article')
+
+
+@app.route('/topics/<topic_name>/categories/<category_name>/<article_id>/')
+def show_article(topic_name, category_name, article_id):
+  print('A particular article')
+
+
+@app.route('/topics/<topic_name>/categories/<category_name>/<article_id>/edit')
+def edit_article(topic_name, category_name, article_id):
+  print('Edit an article')
+
+
+@app.route('/topics/<topic_name>/categories/<category_name>/<article_id>/delete')
+def delete_article(topic_name, category_name, article_id):
+  print('Delete an article')
 
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
 
-# GET /
-# GET /topics
-# GET /topics/AI/
-# GET /topics/AI/category/new
-# GET /topics/AI/category/Python/
-# GET /topics/AI/category/Python/new
-# GET /topics/AI/category/Python/4
-# GET /topics/AI/category/Python/4/edit
+# [X] GET /
+# [X] GET /topics
+# [X] GET /topics/AI/
+# [X] GET /topics/AI/categories/new
+# [X] GET /topics/AI/categories/Python/
+# [X] GET /topics/AI/categories/Python/new
+# [X] GET /topics/AI/categories/Python/4
+# [X] GET /topics/AI/categories/Python/4/edit
+# [X] GET /topics/AI/categories/Python/4/delete
 
 
 # topics
