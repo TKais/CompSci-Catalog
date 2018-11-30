038ebfb42fe305cf91b5a2f030f87e2daaafa8f6
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-11-29 22:02:10 -0800

    feat: Add API endpoints for rendering a single article

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, jsonify
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from db import Base, Topic, Category
+from db import Base, Topic, Category, Article
 
 app = Flask(__name__)
 
@@ -18,7 +18,6 @@ session = DBSession()
 @app.route('/topics/')
 def show_topics():
   topics = session.query(Topic)
-  print(topics)
   return render_template('topics.html', topics=topics)
 
 @app.route('/topics/JSON/')
@@ -28,7 +27,6 @@ def return_topics_JSON():
 
 @app.route('/topics/<topic_url>/')
 def show_topic(topic_url):
-  print('A particular topic page', topic_url)
   topic = session.query(Topic).filter_by(url = topic_url).one()
   categories = session.query(Category).filter_by(topic_id = topic.id).all()
   return render_template('topic.html', topic=topic, categories=categories)
@@ -41,9 +39,9 @@ def create_category(topic_name):
 
 @app.route('/topics/<topic_url>/categories/<category_url>/')
 def show_category(topic_url, category_url):
-  print('A particular category page')
   category = session.query(Category).filter_by(url=category_url).one()
-  return render_template('category.html', category=category)
+  articles = session.query(Article).filter_by(category_id=category.id).all()
+  return render_template('category.html', category=category, articles=articles, topic_url=topic_url)
 
 
 @app.route('/topics/<topic_name>/categories/<category_name>/new/', methods=['GET', 'POST'])
@@ -51,9 +49,11 @@ def create_article(topic_name, category_name):
   print('new article')
 
 
-@app.route('/topics/<topic_name>/categories/<category_name>/<article_id>/')
-def show_article(topic_name, category_name, article_id):
-  print('A particular article')
+@app.route('/topics/<topic_url>/categories/<category_url>/<article_id>/')
+def show_article(topic_url, category_url, article_id):
+  category = session.query(Category).filter_by(url=category_url).one()
+  article = session.query(Article).filter_by(category_id=category.id).one()
+  return render_template('article.html', article=article)
 
 
 @app.route('/topics/<topic_name>/categories/<category_name>/<article_id>/edit')
