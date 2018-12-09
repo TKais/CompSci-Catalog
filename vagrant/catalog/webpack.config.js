f537c478e676a729b2ef6f393b384c95c5721c08
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-12-08 20:21:33 -0800

    feat: Add webpack config file

---
+++ vagrant/catalog/webpack.config.js
@@ -0,0 +1,41 @@
+var UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: [
+    './assets/css/main.scss'
+  ],
+  output: {
+    path: __dirname + '/js',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+};
