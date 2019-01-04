f2159f311c84facd7238ee81d5ae8249cef56d18
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2019-01-03 21:13:58 -0800

    feat: Add a User table to the database

--- vagrant/catalog/db.py
+++ vagrant/catalog/db.py
@@ -64,5 +64,14 @@ class Article(Base):
     }
 
 
+class User(Base):
+  __tablename__ = 'user'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String(250), nullable=False)
+  email = Column(String(250), nullable=False)
+  picture = Column(String(250))
+
+
 engine = create_engine('sqlite:///compscicatalog.db')
 Base.metadata.create_all(engine)
