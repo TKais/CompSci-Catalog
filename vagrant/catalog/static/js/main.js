c4233a0cce04f4b6b148a2556db5d358d2e15890
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2019-01-06 10:44:22 -0800

    feat: Add the new sass file to the bundle

--- vagrant/catalog/static/js/main.js
+++ vagrant/catalog/static/js/main.js
@@ -3,4 +3,5 @@ import '../../assets/css/navbar.scss';
 import '../../assets/css/topic.scss';
 import '../../assets/css/category.scss';
 import '../../assets/css/article.scss';
+import '../../assets/css/login.scss';
 import './signIn';
