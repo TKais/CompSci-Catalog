9eab928fa8af392a316260db7123b371d9a7605b
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2019-01-06 19:37:01 -0800

    chore: Fix pep8 errors

--- vagrant/catalog/db.py
+++ vagrant/catalog/db.py
@@ -7,82 +7,82 @@ Base = declarative_base()
 
 
 class Topic(Base):
-  __tablename__ = 'topic'
+    __tablename__ = 'topic'
 
-  id = Column(Integer, primary_key = True)
-  name = Column(String(250), nullable = False)
-  url = Column(String(250), nullable = False)
-  image = Column(String(250))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    image = Column(String(250))
 
-  @property
-  def serialize(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'url': self.url,
-      'image': self.image
-    }
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'url': self.url,
+            'image': self.image
+        }
 
 
 class Category(Base):
-  __tablename__ = 'category'
+    __tablename__ = 'category'
 
-  id = Column(Integer, primary_key = True)
-  name = Column(String(250), nullable = False)
-  url = Column(String(250), nullable = False)
-  image = Column(String(250))
-  topic_id = Column(Integer, ForeignKey('topic.id'))
-  topic = relationship(Topic)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    image = Column(String(250))
+    topic_id = Column(Integer, ForeignKey('topic.id'))
+    topic = relationship(Topic)
 
-  @property
-  def serialize(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'url': self.url,
-      'image': self.image,
-      'topic_id': self.topic_id,
-    }
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'url': self.url,
+            'image': self.image,
+            'topic_id': self.topic_id,
+        }
 
 
 class User(Base):
-  __tablename__ = 'user'
+    __tablename__ = 'user'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String(250), nullable=False)
-  email = Column(String(250), nullable=False)
-  picture = Column(String(250))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
-  @property
-  def serialize(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'email': self.email,
-      'picture': self.picture,
-    }
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture,
+        }
 
 
 class Article(Base):
-  __tablename__ = 'article'
+    __tablename__ = 'article'
 
-  id = Column(Integer, primary_key = True)
-  name = Column(String(250), nullable = False)
-  content = Column(String(250), nullable = False)
-  category_id = Column(Integer, ForeignKey('category.id'))
-  category = relationship(Category)
-  user_id = Column(Integer, ForeignKey('user.id'))
-  user = relationship(User)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    content = Column(String(250), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-  @property
-  def serialize(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'content': self.content,
-      'category_id': self.category_id,
-      'user_id': self.user_id,
-    }
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'content': self.content,
+            'category_id': self.category_id,
+            'user_id': self.user_id,
+        }
 
 
 engine = create_engine('sqlite:///compscicatalog.db')
